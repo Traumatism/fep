47277eb139ef87731d54176a33d6aebe2c0b3477
toast <58362380+Traumatism@users.noreply.github.com>
2023-07-03 14:26:04 +0200

    Updated code

--- fep/common/module/__init__.py
+++ fep/common/module/__init__.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
+
 from typing import Any, final, Dict
 
 from .utils.encoding import Encoding
@@ -6,14 +7,20 @@ from .utils.http import HTTP
 from .utils.os import Os
 
 
-class Module(Os, HTTP, Encoding, metaclass=ABCMeta):
+class Module(
+    Os,
+    HTTP,
+    Encoding,
+    metaclass=ABCMeta,
+):
     __slots__ = ("env",)
 
     @final
-    def __init__(self, env: Dict[str, Any] = {}) -> None:
+    def __init__(self, env: Dict[str, Any] = dict()) -> None:
         self.env = env
 
-    @abstractproperty
+    @property
+    @abstractmethod
     def data(self) -> Dict[str, str]:
         raise NotImplementedError()
 
